ba5fe18203d24a9fd7a02045198257e9fab303b1
Quentin Bergmann <quentinbergmann@Mac.I-FR.local>
2026-05-12 11:45:17 +0200

    commit2

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPage } from './components/landing-page/landing-page';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LandingPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
