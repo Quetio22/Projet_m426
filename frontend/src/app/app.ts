6bfa9fbac4cb15b0db5c5b51f9f45d2478680611
Quetio22 <Quentin.Bergmann@studentfr.ch>
2026-05-19 08:35:34 +0200

    add landing-page-component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPage } from './components/landing-page/landing-page';
+import { LandingPagePrincipal } from './components/landing-page-principal/landing-page-principal';
 
 
 @Component({
   selector: 'app-root',
-  imports: [LandingPage],
+  imports: [LandingPage, LandingPagePrincipal],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
