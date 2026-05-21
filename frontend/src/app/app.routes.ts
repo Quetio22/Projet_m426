09e65108223262e42a3de9d2d73af2b344bbff36
Quetio22 <Quentin.Bergmann@studentfr.ch>
2026-05-21 11:37:24 +0200

    creation of the register page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPagePrincipal } from './components/landing-page-principal/landing-page-principal';
 import { Login } from './components/login/login';
+import { Register } from './components/register/register';
 
 export const routes: Routes = [
   { path: '', component: LandingPagePrincipal },
   { path: 'login', component: Login },
+  { path: 'register', component: Register },
 ];
